30fcdbaee22186a5f31394049f31fbd1aab24aac
Nathan Esau <nathanesau1@gmail.com>
2020-07-10 18:34:00 -0400

    heroku fix

--- entities/entity.py
+++ entities/entity.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine, Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+engine = create_engine(f'sqlite:///app.db')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
